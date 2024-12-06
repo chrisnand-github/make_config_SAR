ccd83fcd8335a20e3dec70485746b51e924e9599
chrisnand-github <68492454+chrisnand-github@users.noreply.github.com>
2024-12-06 21:02:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,37 @@
 import streamlit as st
+from collections import defaultdict
+import re
+import math
+import os
+import glob
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+def main():
+    # Title of the app
+    st.title("MAKE SAR CONFIG")
+
+    # Create a textbox for input text
+    st.write("PUT IP SYTEM")
+    input_text = st.text_area("Input Text", height=200)
+
+    # Create a button to trigger the conversion
+    if st.button("MAKE SERVICE"):
+        if input_text:
+            # Call the conversion function
+            #output_text = final_service(input_text)
+            output_text = final_service(input_text)
+            # Display the output on the right side
+            st.write("### Converted Text:")
+            st.text_area("Output Text", output_text, height=200)
+            st.download_button(
+                label="Save Result",
+                data=output_text,
+                file_name="converted_text.txt",
+                mime="text/plain"
+            )
+        else:
+            st.write("Please paste some text to convert.")
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()
